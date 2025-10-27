07b4b79b70776a15597beab303e39acafb8a4698
sasha <adganshin@gmail.com>
2025-10-27 11:06:38 +0100

    refactored save_to_file to use AppError and made id atomic utilizing tmp file

--- src/repository/file/file_helper.rs
+++ src/repository/file/file_helper.rs
@@ -1,12 +1,20 @@
-use std::path::Path;
+use crate::util::error::AppResult;
+use std::{fs, path::Path};
 
 pub struct FileHelper {}
 
 impl FileHelper {
-    pub fn save_to_file<P: AsRef<Path>, T: serde::Serialize>(
-        path: &P,
-        data: &T,
-    ) -> std::io::Result<()> {
-        std::fs::write(path, serde_json::to_string_pretty(data)?)
+    pub fn save_to_file<P, T>(path: &P, data: &T) -> AppResult<()>
+    where
+        P: AsRef<Path>,
+        T: serde::Serialize,
+    {
+        let json = serde_json::to_string_pretty(data)?;
+
+        let tmp_path = path.as_ref().with_extension("tmp");
+        fs::write(&tmp_path, json)?;
+        fs::rename(&tmp_path, path)?;
+
+        Ok(())
     }
 }
