d1ba7c8fe0494bad2e446fc8e6d02d2d98b15517
sasha <adganshin@gmail.com>
2025-10-28 09:52:58 +0100

    refactored super category repo accordingly

--- src/repository/file/super_category_repo.rs
+++ src/repository/file/super_category_repo.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use crate::{
     Budget, SuperCategory,
     repository::{file::file_helper::FileHelper, traits::SuperCategoryRepository},
+    util::error::AppResult,
 };
 
 pub struct FileSuperCategoryRepo {
@@ -11,17 +12,19 @@ pub struct FileSuperCategoryRepo {
 }
 
 impl FileSuperCategoryRepo {
-    pub fn new(path: PathBuf) -> Self {
-        let data = std::fs::read_to_string(&path)
-            .ok()
-            .and_then(|s| serde_json::from_str(&s).ok())
-            .unwrap_or_default();
+    pub fn new(path: PathBuf) -> AppResult<Self> {
+        let data = if path.exists() {
+            let json = std::fs::read_to_string(&path)?;
+            serde_json::from_str(&json)?
+        } else {
+            Vec::new()
+        };
 
-        Self { path, data }
+        Ok(Self { path, data })
     }
 
-    pub fn persist(&self) {
-        let _ = FileHelper::save_to_file(&self.path, &self.data);
+    pub fn persist(&self) -> AppResult<()> {
+        FileHelper::save_to_file(&self.path, &self.data)
     }
 }
 
@@ -38,18 +41,18 @@ impl SuperCategoryRepository for FileSuperCategoryRepo {
         data.iter().find(|&sc| sc.id == id).cloned()
     }
 
-    fn save(&mut self, super_category: SuperCategory) {
+    fn save(&mut self, super_category: SuperCategory) -> AppResult<()> {
         if let Some(exisiting) = self.data.iter_mut().find(|sc| sc.id == super_category.id) {
             *exisiting = super_category;
         } else {
             self.data.push(super_category);
         }
 
-        self.persist();
+        self.persist()
     }
 
-    fn delete(&mut self, id: uuid::Uuid) {
+    fn delete(&mut self, id: uuid::Uuid) -> AppResult<()> {
         self.data.retain(|sc| sc.id != id);
-        self.persist();
+        self.persist()
     }
 }
