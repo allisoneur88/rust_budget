5ab6909f8c482233f6c613516db3ef43892d57d7
sasha <adganshin@gmail.com>
2025-10-28 11:24:20 +0100

    refactored user controller to follow consistent error handling api

--- src/controllers/user_controller.rs
+++ src/controllers/user_controller.rs
@@ -2,7 +2,7 @@ use std::{cell::RefCell, rc::Rc};
 
 use uuid::Uuid;
 
-use crate::{User, UserService, app::app_state::AppState};
+use crate::{User, UserService, app::app_state::AppState, util::error::AppResult};
 
 pub struct UserController {
     user_service: UserService,
@@ -17,39 +17,41 @@ impl UserController {
         }
     }
 
-    pub fn get_all(&self) -> Vec<User> {
-        self.app_state.borrow().users.list()
+    pub fn get_all(&self) -> AppResult<Vec<User>> {
+        let state = self.app_state.borrow();
+        Ok(state.users.list())
     }
 
-    pub fn get_by_id(&self, id: Uuid) -> Option<User> {
-        self.app_state.borrow().users.get(id)
+    pub fn get_by_id(&self, id: Uuid) -> AppResult<Option<User>> {
+        let state = self.app_state.borrow();
+        Ok(state.users.get(id))
     }
 
-    pub fn create_with_password(&mut self, name: &str, password: &str) {
+    pub fn create_with_password(&mut self, name: &str, password: &str) -> AppResult<()> {
         let user = self.user_service.make_user(name, password);
-        self.app_state.borrow_mut().users.save(user);
+        self.app_state.borrow_mut().users.save(user)
     }
 
-    pub fn create_without_password(&mut self, name: &str) {
+    pub fn create_without_password(&mut self, name: &str) -> AppResult<()> {
         let user = self.user_service.make_user_wo_password(name);
-        self.app_state.borrow_mut().users.save(user);
+        self.app_state.borrow_mut().users.save(user)
     }
 
-    pub fn update_name(&mut self, user: User, new_name: &str) {
+    pub fn update_name(&mut self, user: User, new_name: &str) -> AppResult<()> {
         let mut user = self.app_state.borrow().users.get(user.id).unwrap();
         self.user_service.update_user_name(&mut user, new_name);
-        self.app_state.borrow_mut().users.save(user);
+        self.app_state.borrow_mut().users.save(user)
     }
 
-    pub fn update_password(&mut self, user: User, new_password: &str) {
+    pub fn update_password(&mut self, user: User, new_password: &str) -> AppResult<()> {
         let mut user = self.app_state.borrow().users.get(user.id).unwrap();
         self.user_service
             .update_user_password(&mut user, new_password);
-        self.app_state.borrow_mut().users.save(user);
+        self.app_state.borrow_mut().users.save(user)
     }
 
-    pub fn delete(&mut self, user: User) {
+    pub fn delete(&mut self, user: User) -> AppResult<()> {
         let user = self.app_state.borrow().users.get(user.id).unwrap();
-        self.app_state.borrow_mut().users.delete(user.id);
+        self.app_state.borrow_mut().users.delete(user.id)
     }
 }
