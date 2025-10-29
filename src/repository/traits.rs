d949409c219ff8d33a36595f5238abce0dd6f09d
sasha <adganshin@gmail.com>
2025-10-29 13:26:20 +0100

    completely refactored traits.rs, aiming for stateless repos, io on each action, no global shared state

--- src/repository/traits.rs
+++ src/repository/traits.rs
@@ -6,71 +6,71 @@ use crate::{
 };
 
 pub trait UserRepository {
-    fn list(&self) -> Vec<User>;
-    fn get(&self, id: Uuid) -> Option<User>;
-    fn save(&mut self, user: User) -> AppResult<()>;
-    fn delete(&mut self, id: Uuid) -> AppResult<()>;
+    fn list(&self) -> AppResult<Vec<User>>;
+    fn get(&self, id: Uuid) -> AppResult<Option<User>>;
+    fn save(&self, user: &User) -> AppResult<()>;
+    fn delete(&self, id: Uuid) -> AppResult<()>;
 }
 
 pub trait BudgetRepository {
-    fn list(&self, user: &User) -> Vec<Budget>;
-    fn get(&self, user: &User, id: Uuid) -> Option<Budget>;
-    fn save(&mut self, budget: Budget) -> AppResult<()>;
-    fn delete(&mut self, id: Uuid) -> AppResult<()>;
+    fn list(&self, user_id: Uuid) -> AppResult<Vec<Budget>>;
+    fn get(&self, id: Uuid) -> AppResult<Option<Budget>>;
+    fn save(&self, budget: &Budget) -> AppResult<()>;
+    fn delete(&self, id: Uuid) -> AppResult<()>;
 }
 
 pub trait AccountRepository {
-    fn list(&self, budget: &Budget) -> Vec<Account>;
-    fn get(&self, budget: &Budget, id: Uuid) -> Option<Account>;
-    fn save(&mut self, account: Account) -> AppResult<()>;
-    fn delete(&mut self, id: Uuid) -> AppResult<()>;
+    fn list(&self, budget_id: Uuid) -> AppResult<Vec<Account>>;
+    fn get(&self, id: Uuid) -> AppResult<Option<Account>>;
+    fn save(&self, account: &Account) -> AppResult<()>;
+    fn delete(&self, id: Uuid) -> AppResult<()>;
 }
 
 pub trait SuperCategoryRepository {
-    fn list(&self, budget: &Budget) -> Vec<SuperCategory>;
-    fn get(&self, budget: &Budget, id: Uuid) -> Option<SuperCategory>;
-    fn save(&mut self, super_category: SuperCategory) -> AppResult<()>;
-    fn delete(&mut self, id: Uuid) -> AppResult<()>;
+    fn list(&self, budget_id: Uuid) -> AppResult<Vec<SuperCategory>>;
+    fn get(&self, id: Uuid) -> AppResult<Option<SuperCategory>>;
+    fn save(&self, super_category: &SuperCategory) -> AppResult<()>;
+    fn delete(&self, id: Uuid) -> AppResult<()>;
 }
 
 pub trait CategoryRepository {
-    fn list(&self) -> Vec<Category>;
-    fn get(&self, id: Uuid) -> Option<Category>;
-    fn save(&mut self, category: Category) -> AppResult<()>;
-    fn delete(&mut self, id: Uuid) -> AppResult<()>;
+    fn list(&self, super_category_id: Uuid) -> AppResult<Vec<Category>>;
+    fn get(&self, id: Uuid) -> AppResult<Option<Category>>;
+    fn save(&self, category: &Category) -> AppResult<()>;
+    fn delete(&self, id: Uuid) -> AppResult<()>;
 }
 
 pub trait AssignmentRepository {
-    fn list(&self) -> Vec<Assignment>;
-    fn get(&self, id: Uuid) -> Option<Assignment>;
-    fn save(&mut self, assignment: Assignment) -> AppResult<()>;
-    fn delete(&mut self, id: Uuid) -> AppResult<()>;
+    fn list(&self, category_id: Uuid) -> AppResult<Vec<Assignment>>;
+    fn get(&self, id: Uuid) -> AppResult<Option<Assignment>>;
+    fn save(&self, assignment: &Assignment) -> AppResult<()>;
+    fn delete(&self, id: Uuid) -> AppResult<()>;
 }
 
 pub trait SuperTransactionRepository {
-    fn list(&self, budget: &Budget) -> Vec<SuperTransaction>;
-    fn get(&self, budget: &Budget, id: Uuid) -> Option<SuperTransaction>;
-    fn save(&mut self, super_transaction: SuperTransaction) -> AppResult<()>;
-    fn delete(&mut self, id: Uuid) -> AppResult<()>;
+    fn list(&self, account_id: Uuid) -> AppResult<Vec<SuperTransaction>>;
+    fn get(&self, id: Uuid) -> AppResult<Option<SuperTransaction>>;
+    fn save(&self, super_transaction: &SuperTransaction) -> AppResult<()>;
+    fn delete(&self, id: Uuid) -> AppResult<()>;
 }
 
 pub trait TransactionRepository {
-    fn list(&self) -> Vec<Transaction>;
-    fn get(&self, id: Uuid) -> Option<Transaction>;
-    fn save(&mut self, transaction: Transaction) -> AppResult<()>;
-    fn delete(&mut self, id: Uuid) -> AppResult<()>;
+    fn list(&self, super_transaction_id: Uuid) -> AppResult<Vec<Transaction>>;
+    fn get(&self, id: Uuid) -> AppResult<Option<Transaction>>;
+    fn save(&self, transaction: &Transaction) -> AppResult<()>;
+    fn delete(&self, id: Uuid) -> AppResult<()>;
 }
 
 pub trait PayeeRepository {
-    fn list(&self) -> Vec<Payee>;
-    fn get(&self, id: Uuid) -> Option<Payee>;
-    fn save(&mut self, payee: Payee) -> AppResult<()>;
-    fn delete(&mut self, id: Uuid) -> AppResult<()>;
+    fn list(&self, budget_id: Uuid) -> AppResult<Vec<Payee>>;
+    fn get(&self, id: Uuid) -> AppResult<Option<Payee>>;
+    fn save(&self, payee: &Payee) -> AppResult<()>;
+    fn delete(&self, id: Uuid) -> AppResult<()>;
 }
 
 pub trait CurrencyRepository {
-    fn list(&self) -> Vec<Currency>;
-    fn get(&self, id: Uuid) -> Option<Currency>;
-    fn save(&mut self, currency: Currency) -> AppResult<()>;
-    fn delete(&mut self, id: Uuid) -> AppResult<()>;
+    fn list(&self, budget_id: Uuid) -> AppResult<Vec<Currency>>;
+    fn get(&self, id: Uuid) -> AppResult<Option<Currency>>;
+    fn save(&self, currency: &Currency) -> AppResult<()>;
+    fn delete(&self, id: Uuid) -> AppResult<()>;
 }
