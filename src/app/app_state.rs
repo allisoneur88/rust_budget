98f7cf0335d764d2434b7a128a420771d77b44d8
sasha <adganshin@gmail.com>
2025-10-30 11:37:52 +0100

    refactored app_state to only keep session-related state data

--- src/app/app_state.rs
+++ src/app/app_state.rs
@@ -1,67 +1,58 @@
-use crate::{
-    Account, AccountType, Budget, User,
-    app::app_config::AppConfig,
-    repository::{
-        file::{
-            account_repo::FileAccountRepo, assignment_repo::FileAssignmentRepo,
-            budget_repo::FileBudgetRepo, category_repo::FileCategoryRepo,
-            currency_repo::FileCurrencyRepo, super_category_repo::FileSuperCategoryRepo,
-            super_transaction_repo::FileSuperTransactionRepo,
-            transaction_repo::FileTransactionRepo, user_repo::FileUserRepo,
-        },
-        traits::{
-            AccountRepository, AssignmentRepository, BudgetRepository, CategoryRepository,
-            CurrencyRepository, SuperCategoryRepository, SuperTransactionRepository,
-            TransactionRepository, UserRepository,
-        },
-    },
-    util::error::AppResult,
-};
+use std::env;
+
+use uuid::Uuid;
+
+use crate::util::error::{AppError, AppResult};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct AppState {
-    pub version: u32,
-    pub users: Box<dyn UserRepository>,
-    pub budgets: Box<dyn BudgetRepository>,
-    pub accounts: Box<dyn AccountRepository>,
-    pub super_categories: Box<dyn SuperCategoryRepository>,
-    pub super_transactions: Box<dyn SuperTransactionRepository>,
-    pub categories: Box<dyn CategoryRepository>,
-    pub transactions: Box<dyn TransactionRepository>,
-    pub assignments: Box<dyn AssignmentRepository>,
-    pub currencies: Box<dyn CurrencyRepository>,
-    pub account_types: Vec<AccountType>,
+    pub version: &'static str,
 
-    pub current_user: Option<User>,
-    pub current_budget: Option<Budget>,
-    pub current_account: Option<Account>,
+    pub current_user_id: Option<Uuid>,
+    pub current_budget_id: Option<Uuid>,
+    pub current_account_id: Option<Uuid>,
 }
 
 impl AppState {
-    pub fn new() -> AppResult<Self> {
-        let config = AppConfig::load("config.toml").unwrap();
+    pub fn new() -> Self {
+        Self {
+            version: VERSION,
+            current_user_id: None,
+            current_budget_id: None,
+            current_account_id: None,
+        }
+    }
 
-        Ok(Self {
-            version: 1,
-            users: Box::new(FileUserRepo::new(config.user_path)?),
-            budgets: Box::new(FileBudgetRepo::new(config.budget_path)?),
-            accounts: Box::new(FileAccountRepo::new(config.account_path)?),
-            super_categories: Box::new(FileSuperCategoryRepo::new(config.super_category_path)?),
-            super_transactions: Box::new(FileSuperTransactionRepo::new(
-                config.super_transaction_path,
-            )?),
-            categories: Box::new(FileCategoryRepo::new(config.category_path)?),
-            transactions: Box::new(FileTransactionRepo::new(config.transaction_path)?),
-            assignments: Box::new(FileAssignmentRepo::new(config.assignment_path)?),
-            currencies: Box::new(FileCurrencyRepo::new(config.currency_path)?),
-            account_types: vec![
-                AccountType::Checking,
-                AccountType::CreditCard,
-                AccountType::Investment,
-            ],
+    pub fn login(&mut self, user_id: Uuid) {
+        self.current_user_id = Some(user_id);
 
-            current_user: None,
-            current_budget: None,
-            current_account: None,
-        })
+        self.current_budget_id = None;
+        self.current_account_id = None;
+    }
+
+    pub fn logout(&mut self) {
+        *self = Self::new();
+    }
+
+    pub fn select_budget(&mut self, budget_id: Uuid) {
+        self.current_budget_id = Some(budget_id);
+        self.current_account_id = None;
+    }
+
+    pub fn select_account(&mut self, account_id: Uuid) {
+        self.current_account_id = Some(account_id);
+    }
+
+    pub fn require_user(&self) -> AppResult<Uuid> {
+        self.current_user_id.ok_or(AppError::NotLoggedIn)
+    }
+
+    pub fn require_budget(&self) -> AppResult<Uuid> {
+        self.current_budget_id.ok_or(AppError::NoBudgetSelected)
+    }
+
+    pub fn require_account(&self) -> AppResult<Uuid> {
+        self.current_account_id.ok_or(AppError::NoAccountSelected)
     }
 }
